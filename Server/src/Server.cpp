df641f04a30afc534974b83d788e76fac1377aa4
userxg <numberuserDinfo@gmail.com>
2024-05-05 15:38:53 +0300

    server validation (n)

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -8,7 +8,33 @@ Server::Server(unsigned short port) : listened_port_(port)
 	else LOG("Server started");
 }
 
-void Server::ConnectClients()
+bool Server::NameIsTaken(const std::string& checked_name)
+{
+	for (const auto& client : clients_)
+	{
+		if (client->name == checked_name)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Server::ClientExists(const std::string& checked_name)
+{
+	for (const auto& client : clients_)
+	{
+		if (client->name == checked_name)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Server::ConnectIncomingClients()
 {
 	while (true)
 	{
@@ -18,7 +44,7 @@ void Server::ConnectClients()
 
 		if (accept_status == sf::Socket::Done)
 		{
-			//new_client->setBlocking(false);
+			new_client->socket.setBlocking(false);
 			clients_.push_back(new_client);
 			LOG("Added client " << clients_.size());
 		}
@@ -26,9 +52,114 @@ void Server::ConnectClients()
 	}
 }
 
+void Server::ManageIncomingPackets()
+{
+	while (true)
+	{
+		for (size_t i = 0; i < clients_.size(); ++i)
+		{
+			Client* client = clients_[i];
+
+			sf::Packet received_packet;
+
+			sf::Socket::Status rev_packet_status = client->socket.receive(received_packet);
+
+			switch (rev_packet_status)
+			{
+			case sf::Socket::Done:
+				ProcessReceivedPacket(received_packet, client);
+				break;
+			case sf::Socket::NotReady:
+				break;
+			case sf::Socket::Disconnected:
+				break;
+			case sf::Socket::Error:
+				LOG("ERROR on receiving packet");
+				break;
+			}
+		}
+	}
+}
+
+void Server::ProcessReceivedPacket(sf::Packet& received_packet, Client* client)
+{
+	Message received_msg;
+	received_packet >> received_msg;
+
+	if (received_msg.is_new_client != true)
+	{
+
+		SendToClient(received_msg, client);
+	}
+	else
+	{
+		sf::Packet validation_packet;
+		validation_packet << received_msg;
+
+		if (NameIsTaken(received_msg.name_of_new_client))
+		{
+			received_msg.is_new_client = true;
+			received_msg.name_is_taken = true;
+			SendValidationResponse(validation_packet, client);
+		}
+		{
+			received_msg.is_new_client = false;
+			received_msg.name_is_taken = false;
+			SendValidationResponse(validation_packet, client);
+		}
+	}
+
+
+}
+
 void Server::Run()
 {
-	std::thread incoming_connection_thread(&Server::ConnectClients, this);
+	std::thread incoming_connection_thread(&Server::ConnectIncomingClients, this);
 
 	incoming_connection_thread.join();
+}
+
+
+Client* Server::FindClient(const std::string& name)
+{
+	for (auto& client : clients_)
+	{
+		if (client->name == name)
+		{
+			return client;
+		}
+	}
+	return nullptr;
+}
+
+void Server::SendToClient(Message& send_msg, Client* client)
+{
+	sf::Packet send_packet;
+
+	//Change on Client online
+	Client* client_to = FindClient(send_msg.client_to);
+	if (client_to->online)
+	{
+		
+		send_packet << send_msg;
+		if (client->socket.send(send_packet) != sf::Socket::Done)
+		{
+			LOG("Cound't send packet");
+		}
+	}
+	else
+	{
+
+		//send_msg << 
+		//send_packet << send_msg;
+
+	}
+}
+
+void Server::SendValidationResponse(sf::Packet& send_packet, Client* client)
+{
+	if (client->socket.send(send_packet) != sf::Socket::Done)
+	{
+		LOG("Cound't send packet");
+	}
 }
--- Server/src/Server.h
+++ Server/src/Server.h
@@ -13,14 +13,41 @@
 
 struct Client
 {	
-	Client(std::string name) : name(name) {}
+	Client(std::string name) : name(name), online(true) {}
 	sf::TcpSocket socket;
 	std::string name;
+	bool online;
 };
+
+struct Message
+{
+	//for new clients
+	bool is_new_client = false;
+	bool name_is_taken = false;
+	std::string name_of_new_client;
+
+	//between existing clients
+	std::string client_from;
+	std::string client_to;
+	std::string message;
+};
+
+
+sf::Packet& operator>>(sf::Packet& inp, Message& msg)
+{
+	inp >> msg.is_new_client >> msg.name_is_taken >> msg.name_of_new_client >> msg.client_from >> msg.client_to >> msg.message;
+	return inp;
+}
+sf::Packet& operator<<(sf::Packet& out, const Message& msg)
+{
+	out << msg.is_new_client << msg.name_is_taken << msg.name_of_new_client << msg.client_from << msg.client_to << msg.message;
+	return out;
+}
 
 class Server
 {
 private:
+	
 	sf::TcpListener listener_;
 	std::vector<Client*> clients_;
 
@@ -28,7 +55,19 @@ private:
 
 
 private:
-	void ConnectClients();
+	void ConnectIncomingClients();
+	void ManageIncomingPackets();
+
+
+	void ProcessReceivedPacket(sf::Packet& received_packet, Client* client);
+	void SendValidationResponse(sf::Packet& validation_packet, Client* client);
+	void SendToClient(Message& send_packet, Client* client);
+	Client* FindClient(const std::string& name);
+	
+	
+	bool ClientExists(const std::string& checked_name);
+	bool NameIsTaken(const std::string& checked_name);
+	
 
 public:
 
